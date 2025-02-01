e848cf905130e83a3d918f597d43d29e443c692b
marco2922 <ashishvika@gmail.com>
2025-02-02 05:14:57 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,4 @@
-// Connect Wallet Button
-document.getElementById('connectWallet').addEventListener('click', async () => {
-  if (window.ethereum) {
-      try {
-          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-          alert(`Connected: ${accounts[0]}`);
-      } catch (error) {
-          console.error(error);
-          alert('Connection failed.');
-      }
-  } else {
-      alert('Please install MetaMask.');
-  }
-});
+
 
 // Submit News Form
 document.getElementById('submitNewsForm').addEventListener('submit', (e) => {
